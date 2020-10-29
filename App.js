8fc78ed67e9cc76910189622df5c9f5716e6946f
Indiana Lora <indiana@ensaludybelleza.com>
2020-10-29 13:32:29 -0400

    Added Menu drawer

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import Icon from 'react-native-vector-icons/Octicons';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  Alert,
+  Button,
+  prompt,
+} from "react-native";
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={() => navigation.toggleDrawer() }
+        title="Menu"
+      />
+    </View>
+  );
+}
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      
+      <Button onPress={() => navigation.goBack()} title="Go Back Home" />
+      <Button onPress={navigation.toggleDrawer() } title="hey"/>
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   
+      <NavigationContainer style={styles.container}>
+        <Drawer.Navigator initialRouteName="Home">
+        
+        <Drawer.Screen name="Rutinas" component={HomeScreen} />
+        <Drawer.Screen name="Alimetacion" component={NotificationsScreen} />
+        </Drawer.Navigator>
+       
+      </NavigationContainer>
+       
+     
+  
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "antiquewhite",
+    alignItems: "left",
+    justifyContent: "left",
   },
 });
