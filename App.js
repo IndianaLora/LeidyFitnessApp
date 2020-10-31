fad360b33e4503577b4153cb6a0e627e1f4a64cb
Indiana Lora <indiana@ensaludybelleza.com>
2020-10-31 15:41:44 -0400

    created the header

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Alimentacion from "./Alimentacion"
 import HomeScreen from "./HomeScreen"
+import Drawex from "./Drawex"
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import Icon from "react-native-vector-icons/Octicons";
@@ -20,55 +21,7 @@ const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer style={styles.container}>
-      <Drawer.Navigator initialRouteName="Home" style={styles.navigator}>
-        <Drawer.Screen name="Rutinas" component={HomeScreen} />
-        <Drawer.Screen name="Alimetacion" component={Alimentacion} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <Drawex/>
   );
 }
 
-
-
-
-
-
-
-
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "antiquewhite",
-    marginTop: 30,
-  },
-  menu: {
-    flex: 1,
-    backgroundColor: "antiquewhite",
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 50,
-  },
-  text: {
-    textAlign: "center",
-    color: "white",
-    backgroundColor: "rosybrown",
-    height: 50,
-    fontSize: 30,
-  },
-  vieX: {
-    backgroundColor: "blue",
-  },
-  image: {
-    alignItems: "center",
-    marginLeft: 100,
-    marginTop: 200,
-    width: 200,
-    height: 300,
-  },
-  navigator: {
-    backgroundColor: "#e9967a",
-  },
-});
--- HomeScreen.js
+++ HomeScreen.js
@@ -1,30 +1,41 @@
 import React from "react";
-import {
-    StyleSheet,
-    Text,
-    View,
-    SafeAreaView,
-    Image,
-    Alert,
-    Button,
-    prompt,
-  } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+// import Constant from "expo-constants";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+// import { constant } from "async";
 function HomeScreen({ navigation }) {
-    return (
-      <SafeAreaView style={styles.container}>
-        <View>
-          <Text style={styles.text} type="static">
-            Leidy Fitness
-          </Text>
-          <Image
-            style={styles.image}
-            source={{
-              uri:
-                "https://image.freepik.com/vector-gratis/dibujos-animados-fitness-salud_24640-25738.jpg",
-            }}
-          />
-        </View>
-      </SafeAreaView>
-    );
-  }
-  export default HomeScreen
+  return (
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          //   marginTop:Constant.statusBarHeight,
+          backgroundColor: "#71bceb",
+          flexDirection: "row",
+          
+        }}
+      >
+        <Entypo name="menu" size={50} color="black" style={styles.icon} />
+        <Text style={styles.text}>Leidy Fitness</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "antiquewhite",
+    marginTop: 28,
+    alignItems: "center",
+  },
+  text: {
+    marginTop: 14,
+    marginRight: 250,
+    color: "white",
+    fontSize:16,
+    
+  },
+  icon: {
+    color: "white",
+  },
+});
+export default HomeScreen;
